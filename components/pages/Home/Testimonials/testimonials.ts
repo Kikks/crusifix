ab031ed7a89f29fdd94d1adadc7c30a492234616
kalDmanie <31763141+kalDmanie@users.noreply.github.com>
2021-12-25 08:13:03 +0100

    Update testimonials.ts

--- components/pages/Home/Testimonials/testimonials.ts
+++ components/pages/Home/Testimonials/testimonials.ts
@@ -2,29 +2,45 @@ export const testimonials = [
 	{
 		rating: 5,
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur explicabo nesciunt, voluptatum minus quia quis.",
-		image: "/assets/images/avatar-1.png",
-		name: "Viola Manisa"
+			"Such an amazing place to have fun, cool games and all working perfectly. Very amazing workers as well.",
+		image: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416230/crusifix/testimonial%20photos/Screenshot_2021-12-25_at_8.08.31_AM_xvqvrc.png",
+		name: "Olatunde Fafolahan"
 	},
 	{
 		rating: 5,
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur explicabo nesciunt, voluptatum minus quia quis.",
+			"The best place to get a proper sim racing game, i love that they have the state of the art equipment to make gaming enjoyable.",
+		image: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416213/crusifix/testimonial%20photos/kalu_drrlba.jpg",
+		name: "Kalu Eke"
+	},
+	{
+		rating: 5,
+		description:
+			"It is very condusive and fun",
 		image: "/assets/images/avatar-2.png",
-		name: "Bryon Arnoldy"
+		name: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416220/crusifix/testimonial%20photos/Screenshot_2021-12-25_at_8.04.32_AM_isxtj0.png"
+	},
+
+	{
+		rating: 5,
+		description:
+			"A fun place with a variety of games for everyone to play and an open terrace for people who want to chill while their friends are game. Very cool environment and if you want to get beaten at pool,just call me.",
+		image: "/assets/images/avatar-1.png",
+		name: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416212/crusifix/testimonial%20photos/parker_osaji_k82yjc.png"
 	},
 	{
 		rating: 5,
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur explicabo nesciunt, voluptatum minus quia quis.",
-		image: "/assets/images/avatar-3.png",
-		name: "Joshua William"
+			"Simply dope, they have everything a gamer needs",
+		image: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416211/crusifix/testimonial%20photos/Oghenekome_qg8vkz.png",
+		name: "Oghenekome Akpoborie"
 	},
+
 	{
 		rating: 5,
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur explicabo nesciunt, voluptatum minus quia quis.",
-		image: "/assets/images/avatar-4.png",
-		name: "George Scott"
+			"Perfect!, just what i have been looking for",
+		image: "https://res.cloudinary.com/dh2w0dqug/image/upload/v1640416216/crusifix/testimonial%20photos/Screenshot_2021-12-25_at_8.02.28_AM_mx1mej.png",
+		name: "Uche Austin"
 	}
 ];
